ca7dd7d40c3bc000369bf60ec0e750802095acc4
Flaviu <flaviumircia15@gmail.com>
2024-04-19 19:53:41 +0300

    Feature: Add date of birth and image uri to database

--- src/main/java/com/syncpeer/authapi/configs/SecurityConfig.java
+++ src/main/java/com/syncpeer/authapi/configs/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.syncpeer.authapi.utils.Constants.*;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -26,7 +28,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
                         auth
-                                .requestMatchers("api/v1/auth/**")
+                                .requestMatchers(API_BASE_PATH + AUTH+"/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
---
+++ src/main/java/com/syncpeer/authapi/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.syncpeer.authapi.utils;
+
+public class Constants {
+    public final static String API_BASE_PATH = "api/v1";
+    public final static String AUTH = "/auth";
+    public final static String CREATE_PROFILE = "/create-profile";
+}
